16014b7023fa5181bc52209bf98076313bc3bb05
govind-up <govind@urbanpiper.com>
2019-12-12 15:01:25 +0530

    TimeStampedModel model imported from helper Catalog model added descripton field removed from store

--- apps/merchant/models.py
+++ apps/merchant/models.py
@@ -1,11 +1,5 @@
 from django.db import models
-
-class TimeStampedModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
+from helper.models import TimeStampedModel
 
 # Create your models here.
 class Merchant(TimeStampedModel):
@@ -19,9 +13,17 @@ class Store(TimeStampedModel):
     merchant_id = models.ForeignKey(Merchant, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     address = models.TextField()
-    description = models.TextField()
     lat = models.DecimalField(max_digits=9, decimal_places=6),
     lon = models.DecimalField(max_digits=9, decimal_places=6),
     
     def __str__(self):
         return self.name
+
+class Catalog(TimeStampedModel):
+    merchant_id = models.ForeignKey(Merchant, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=9, decimal_places=6),
+    
+    def __str__(self):
+        return self.title
