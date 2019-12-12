b0775974ed90a17644f99d71c8e006d9c9396daf
nura <arun@urbanpiper.com>
2019-12-12 18:06:42 +0530

    1. setup mysql connectors. 2. add schema migrations for merchant and order apps. 3. add auth layer.
    Fixes #12 #3 #4

--- transaction/urls.py
+++ transaction/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # apps
-    path('api/order/', include('order.urls')),
+    path('api/order/', include('apps.order.urls')),
 ]
