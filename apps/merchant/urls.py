61ca805235b8ad2a0c14c3bdf8fae4d8622f2e5f
govind-up <govind@urbanpiper.com>
2019-12-19 11:48:30 +0530

    CRUD endpoints added for merchant

--- apps/merchant/urls.py
+++ apps/merchant/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from order.views import OrderStatus
+from apps.merchant.views import MerchantListCreate, MerchantRetrieveUpdateDestro, MerchantAPIView, MerchantDetailAPIView, MerchantGenericView
 
 urlpatterns = [
     # TODO: remove this
-    path('dummy-view/', OrderStatus.as_view(), name='order_status'),
+    # path("", MerchantListCreate.as_view()),
+    # path("<int:pk>", MerchantRetrieveUpdateDestro.as_view()),
+    # path("", MerchantAPIView.as_view()),
+    # path("<int:id>", MerchantDetailAPIView.as_view()),
+    path("", MerchantGenericView.as_view()),
+    path("<int:id>", MerchantGenericView.as_view()),
 ]
