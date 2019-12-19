24587f863d42ccd4508209ff5dccccde9479a832
govind-up <govind@urbanpiper.com>
2019-12-19 11:53:00 +0530

    removed unused urls and imports

--- apps/merchant/urls.py
+++ apps/merchant/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from apps.merchant.views import MerchantListCreate, MerchantRetrieveUpdateDestro, MerchantAPIView, MerchantDetailAPIView, MerchantGenericView
+from apps.merchant.views import MerchantGenericView
 
 urlpatterns = [
-    # TODO: remove this
-    # path("", MerchantListCreate.as_view()),
-    # path("<int:pk>", MerchantRetrieveUpdateDestro.as_view()),
-    # path("", MerchantAPIView.as_view()),
-    # path("<int:id>", MerchantDetailAPIView.as_view()),
     path("", MerchantGenericView.as_view()),
     path("<int:id>", MerchantGenericView.as_view()),
 ]
